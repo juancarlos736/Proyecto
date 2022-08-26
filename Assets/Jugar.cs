918804d2a34ad94886d646f0c95c10b44e92d7f6
Christian Fernandez <christian.fernandez@epn.edu.ec>
2022-08-25 22:38:29 -0500

    //updateScena1

---
+++ Assets/Jugar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jugar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public void Jugar() {
+            SceneManagement.LoadScene();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
