fb264420bcdbffd852a57b99842ac607a2f6abf8
Christian Fernandez <christian.fernandez@epn.edu.ec>
2022-09-06 18:21:03 -0500

    //FinalScene

--- Assets/Jugar.cs
+++ Assets/Jugar.cs
@@ -8,8 +8,8 @@ public class Jugar : MonoBehaviour
     
     public void play()
     {
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        //SceneManager.LoadScene(0);
     }
 
     public void exit()
