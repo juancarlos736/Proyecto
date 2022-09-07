b5f3cc23f4560fb1289c26aefce3fa3f50bffc12
Juan Carlos <juancarlosrom1992@gmail.com>
2022-09-07 01:50:57 -0500

    commit

--- Assets/Login.cs
+++ Assets/Login.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Login : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Servidor servidor;
+    public InputField usuario;
+    public InputField contrasenia;
 
-    // Update is called once per frame
-    void Update()
+    public void IniciarSesion()
     {
-        
+        string[] datos = new string[2];
+        datos[0] = usuario.text;
+        datos[1] = contrasenia.text;
+
+        StartCoroutine(servidor.ConsumirServicio("login",datos));
     }
 }
--- Assets/Servidor.cs
+++ Assets/Servidor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 
 [CreateAssetMenu(fileName = "Servidor", menuName = "Morion/Servidor", order = 1)]
@@ -8,6 +9,60 @@ using UnityEngine;
 public class Servidor : ScriptableObject
 {
    public string servidor1;
+   public Servicio[] servicios;
+   public bool ocupado = false;
+   public Respuesta respuesta;
+
+
+   public IEnumerator ConsumirServicio(string nombre, string[] datos){
+      ocupado = true;
+      WWWForm formulario = new WWWForm();
+      Servicio s = new Servicio();
+      for (int i = 0; i< servicios.Length; i++){
+         if (servicios[i].nombre.Equals(nombre)){
+            s=servicios[i];
+         }
+
+      }
+      for (int i = 0; i< s.parametros.Length; i++){
+         formulario.AddField(s.parametros[i],datos[i]);
+
+      }
+      UnityWebRequest www = UnityWebRequest.Post(servidor1+"/"+s.URL, formulario);
+      Debug.Log(servidor1 + "/" + s.URL);
+      yield return www.SendWebRequest();
+
+      if (www.result != UnityWebRequest.Result.Success){
+         respuesta = new Respuesta();
+      }
+      else{
+         Debug.Log(www.downloadHandler.text);
+         respuesta = JsonUtility.FromJson<Respuesta>(www.downloadHandler.text);
+      }
+      ocupado=false;
+
+   }
 
     
 }
+
+[System.Serializable]
+
+public class Servicio{
+   public string nombre;
+   public string URL;
+   public string[] parametros;
+}
+
+
+[System.Serializable]
+
+public class Respuesta{
+   public int codigo;
+   public string mensaje;
+   public Respuesta(){
+      codigo=404;
+      mensaje="Error";
+   }
+}
+
