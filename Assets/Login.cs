33b1636777f2d0886a311087dacba78ba8b8e827
Christian Fernandez <christian.fernandez@epn.edu.ec>
2022-09-07 13:34:06 -0500

    //update

--- Assets/Login.cs
+++ Assets/Login.cs
@@ -6,21 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class Login : MonoBehaviour
 {
-    public Servidor servidor;
+    //public Servidor servidor;
     public InputField usuario;
     public InputField contrasenia;
 
     public void IniciarSesion()
     {
-        string[] datos = new string[2];
+        //string usr = usuario.text;
+        //string cont = contrasenia.text;
+        /*string[] datos = new string[2];
         datos[0] = usuario.text;
         datos[1] = contrasenia.text;
-
-        StartCoroutine(servidor.ConsumirServicio("login",datos, PosCargar));
+         
+        StartCoroutine(servidor.ConsumirServicio("login",datos, PosCargar));*/
+        //if (usr.Equals("usuario1") && cont.Equals("usuario1"))
+        //{
+            SceneManager.LoadScene(1);
+        //}
         
     }
 
-    void PosCargar(){
+    /*void PosCargar(){
         switch (servidor.respuesta.codigo){
             case 0:
                 puntos.puntaje=servidor.respuesta.codigo;
@@ -29,5 +35,5 @@ public class Login : MonoBehaviour
                       
         }
             
-    }
+    }*/
 }
